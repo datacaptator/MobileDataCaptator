f14a42aaaaab32a8384219257806ce59cbd4c7fb
bartvanhoey <bartvanhoey@hotmail.com>
2014-01-17 22:09:41 +0100

    ongebruikte import verwijderd

--- src/be/mobiledatacaptator/activities/ProjectActivity.java
+++ src/be/mobiledatacaptator/activities/ProjectActivity.java
@@ -8,7 +8,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources.NotFoundException;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
